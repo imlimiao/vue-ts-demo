315c6d2a1d4f3b48e6426645d2f7830922268fa3
limiao04 <limiao04@58.com>
2019-11-11 17:44:17 +0800

    Ts demo

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="我在测试---HelloWorld" />
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from "@/components/HelloWorld.vue";
+
+    export default {
+        name: "home",
+        components: {
+            HelloWorld
+        }
+    };
+</script>
